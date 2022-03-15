20bf7219fd61a21121db519a3a17c8163eccd824
Bård Magnus Kvalheim <bmkva@nets.eu>
2022-03-15 16:53:58 +0100

    initial

---
+++ src/test/java/eu/nets/uni/apps/settlement/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.nets.uni.apps.settlement.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
