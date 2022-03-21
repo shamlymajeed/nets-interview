197a0bebe0152d203368fd883e5cde5dd9fe3bf4
Shamly Majeed <shamlymajeed08@gmail.com>
2022-03-22 00:07:24 +0100

    Fix not showing error message issue
    - Error message was not shown because http status set was NO_CONTENT
    - Also set content type to application/json in advice

--- src/main/java/eu/nets/uni/apps/settlement/interview/controller/ControllerExceptionHandler.java
+++ src/main/java/eu/nets/uni/apps/settlement/interview/controller/ControllerExceptionHandler.java
@@ -3,8 +3,9 @@ package eu.nets.uni.apps.settlement.interview.controller;
 import eu.nets.uni.apps.settlement.interview.exception.ErrorResponse;
 import eu.nets.uni.apps.settlement.interview.exception.ExchangeRateNotAvailableException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
@@ -12,12 +13,11 @@ import org.springframework.web.context.request.WebRequest;
 public class ControllerExceptionHandler {
 
   @ExceptionHandler(value = {ExchangeRateNotAvailableException.class})
-  @ResponseStatus(value = HttpStatus.NO_CONTENT)
-  public ErrorResponse exchangeRateNotAvailableException(ExchangeRateNotAvailableException ex , WebRequest request){
+  public ResponseEntity<ErrorResponse> exchangeRateNotAvailableException(ExchangeRateNotAvailableException ex , WebRequest request){
 
     ErrorResponse errorResponse = new ErrorResponse();
     errorResponse.setMessage("Exchange rate for the given currency is not available.");
-    return errorResponse;
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON).body(errorResponse);
   }
 
 }
